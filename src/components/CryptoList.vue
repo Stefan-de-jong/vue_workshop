673a7555c56bf82fab656162e8a3b92fd5606688
Stefan <sjf.de.jong@gmail.com>
2020-09-24 11:15:55 +0200

    initial commit

---
+++ src/components/CryptoList.vue
@@ -0,0 +1,38 @@
+<template>
+  <table class="table">
+    <thead>
+      <td>Name</td>
+      <td>Price</td>
+    </thead>
+    <tbody v-for="crypto in currencies" :key="crypto.name">
+      <tr>
+        <td>{{ crypto.name }}</td>
+        <td>{{ crypto.price }}</td>
+      </tr>
+    </tbody>
+    <button @click="changeValue()">Click meeeeeeee</button>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "CryptoList",
+  data() {
+    return {
+      currencies: [
+        { name: "Bitcoin", price: 9000 },
+        { name: "Ethereum", price: 6541 },
+        { name: "Dogecoin", price: 1242 },
+        { name: "Chainlink", price: 7568 },
+        { name: "Ripple", price: 1783 },
+      ],
+    };
+  },
+  methods: {
+    changeValue() {},
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/DemoComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{ studentname }}</h1>
+    <p v-html="htmltest"></p>
+    <p>{{ htmltest }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DemoComponent",
+  data() {
+    return {
+      studentname: "Stefan",
+      age: 34,
+      htmltest: "<b>This is bold<b>",
+    };
+  },
+  props: {
+    message: String,
+  },
+  methods: {
+    doSomething() {
+      alert(this.studentname);
+    },
+  },
+  mounted() {
+    alert("Fully loaded!");
+  },
+};
+</script>
+
+<style>
+</style>
